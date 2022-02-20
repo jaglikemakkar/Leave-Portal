7de5a428a9d7e53dfb06891979b1ab96894bbb15
jaglikemakkar <jaglike.makkar@gmail.com>
2022-02-20 12:42:44 +0530

    added react files

---
+++ react_app/frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import httpClient from "./httpClient";
+import Intro from "./components/Intro.js"
+import Dashboard from "./components/Dashboard.js"
+import LoginForm from "./components/LoginForm.js"
+
+import Navbar from './components/Navbar.js'
+import './App.css';
+
+function App() {
+  const [user, setUser] = useState(new Map());
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await httpClient.get("//localhost:5000/@me");
+        setUser(resp.data);
+        console.log("USERRRRRRR", user)
+      } catch (error) {
+        console.log("Not authenticated");
+      }
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar user = {user} />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Intro />} />
+        <Route path="/login" exact element={<LoginForm />} />
+        <Route path="/dashboard" exact element={<Dashboard />} />
+        {/* <Route component={NotFound} /> */}
+      </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
