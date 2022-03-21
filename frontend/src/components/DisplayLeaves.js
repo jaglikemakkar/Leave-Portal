4bb6677c79b0b3df2b07f1c43a99e446cc48ecc3
jaglikemakkar <jaglike.makkar@gmail.com>
2022-03-21 18:08:30 +0530

    added save pdf

--- frontend/src/components/DisplayLeaves.js
+++ frontend/src/components/DisplayLeaves.js
@@ -4,6 +4,9 @@ import { useState, useEffect } from 'react'
 import httpClient from "../httpClient";
 import '../css/DisplayLeaves.css';
 import Table from './Table.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPrint } from '@fortawesome/free-solid-svg-icons'
+import jsPDF from 'jspdf';
 
 export default function DisplayLeaves() {
 
@@ -32,6 +35,17 @@ export default function DisplayLeaves() {
       temp.push([x[i].id, x[i].nature, x[i].start_date.slice(0, -12), x[i].duration, x[i].status]);
     }
     setData(temp);
+  }
+
+  const saveLeave = (leave_id) => {
+    let doc = new jsPDF("portrait", "pt", "a3");
+    doc.html(document.querySelector("#modal-" + leave_id + " .table"), {
+      width: 200,
+
+      callback: function (pdf) {
+        pdf.save("leave-" + leave_id + ".pdf");
+      }
+    });
   }
 
   useEffect(() => {
@@ -83,52 +97,58 @@ export default function DisplayLeaves() {
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title" id="exampleModalLabel">Leave Details</h5>
+                <div style={{ "margin": "3px 10px" }}>
+                  <FontAwesomeIcon icon={faPrint} />
+                </div>
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
               <div className="modal-body">
                 <table className='table'>
-                  <tr>
-                    <td><b>Leave Id:</b></td>
-                    <td>{leave.id}</td>
-                  </tr>
-                  <tr>
-                    <td><b>Nature of leave: </b></td>
-                    <td>{leave.nature}</td>
+                  <tbody>
+                    <tr>
+                      <td><b>Leave Id:</b></td>
+                      <td>{leave.id}</td>
+                    </tr>
+                    <tr>
+                      <td><b>Nature of leave: </b></td>
+                      <td>{leave.nature}</td>
 
-                  </tr>
-                  <tr>
-                    <td><b>Start Date of leave: </b></td>
-                    <td>{leave.start_date.slice(0, -12)}</td>
-                  </tr>
-                  <tr>
-                    <td><b>End Date of leave: </b></td>
-                    <td>{leave.end_date.slice(0, -12)}</td>
-                  </tr>
-                  <tr>
-                    <td><b>Duration of leave: </b></td>
-                    <td>{leave.duration}</td>
-                  </tr>
-                  <tr>
-                    <td><b>Request Date of leave: </b></td>
-                    <td>{leave.request_date.slice(0, -12)}</td>
-                  </tr>
-                  <tr>
-                    <td><b>Purpose of leave: </b></td>
-                    <td>{leave.purpose}</td>
-                  </tr>
-                  <tr>
-                    <td><b>Status: </b> </td>
-                    <td>{leave.status}</td>
-                  </tr>
-                  <tr>
-                    <td><b>Authority Comment: </b></td>
-                    <td>{leave.authority_comment}</td>
-                  </tr>
+                    </tr>
+                    <tr>
+                      <td><b>Start Date of leave: </b></td>
+                      <td>{leave.start_date.slice(0, -12)}</td>
+                    </tr>
+                    <tr>
+                      <td><b>End Date of leave: </b></td>
+                      <td>{leave.end_date.slice(0, -12)}</td>
+                    </tr>
+                    <tr>
+                      <td><b>Duration of leave: </b></td>
+                      <td>{leave.duration}</td>
+                    </tr>
+                    <tr>
+                      <td><b>Request Date of leave: </b></td>
+                      <td>{leave.request_date.slice(0, -12)}</td>
+                    </tr>
+                    <tr>
+                      <td><b>Purpose of leave: </b></td>
+                      <td>{leave.purpose}</td>
+                    </tr>
+                    <tr>
+                      <td><b>Status: </b> </td>
+                      <td>{leave.status}</td>
+                    </tr>
+                    <tr>
+                      <td><b>Authority Comment: </b></td>
+                      <td>{leave.authority_comment}</td>
+                    </tr>
+                  </tbody>
                 </table>
               </div>
               <div className="modal-footer">
+                <button type="button" className="btn btn-primary" onClick={() => saveLeave(leave.id)}>Download PDF</button>
                 <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
                 {/* <button type="button" className="btn btn-primary">Save changes</button> */}
               </div>
